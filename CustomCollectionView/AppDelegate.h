e6721e5505e5ffd814619e8021c6427028b20b6f
liansenyao <18668196356@163.com>
2016-07-19 17:14:10 +0800

    Initial Commit

---
+++ CustomCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomCollectionView
+//
+//  Created by xiaoqikeji on 16/7/19.
+//  Copyright © 2016年 xiaoqikeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomCollectionView
+//
+//  Created by xiaoqikeji on 16/7/19.
+//  Copyright © 2016年 xiaoqikeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
